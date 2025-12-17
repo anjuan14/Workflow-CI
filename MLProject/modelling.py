9309e302527410a822b4b870faa8958ee264d29a
anjuan14 <anjuan14>
2025-12-18 00:47:07 +0700

    another test

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+
 # DAGSHUB + MLFLOW INIT
 # dagshub.init(
 #     repo_owner="anjuan14",
@@ -29,6 +30,10 @@ import seaborn as sns
 # mlflow.set_tracking_uri("http://127.0.0.1:5000")
 # mlflow.set_experiment("Heart Disease Modelling - Advance")
 
+mlflow.set_tracking_uri(
+    f"file:{os.path.join(os.getcwd(), 'mlruns')}"
+)
+mlflow.set_experiment("default")
 
 # LOAD DATA
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
